b248a6a5f3b12b95c4d548be55007b70b93de870
Ismayil Hasanov <ihasanov@code.edu.az>
2021-10-27 13:57:37 +0300

    initial commit

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private SpriteRenderer rend;
+    public Sprite[] tileGraphics;
+
+    public float hoverAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        int randTile = Random.Range(0, tileGraphics.Length);
+        rend.sprite = tileGraphics[randTile];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.localScale += Vector3.one * hoverAmount;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale -= Vector3.one * hoverAmount;
+    }
+}
